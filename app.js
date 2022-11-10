eed664e30666a7630570698882519ae17b15348a
David Daza <drdaza18@gmail.com>
2022-11-10 10:58:41 +0100

    save 1

---
+++ app.js
@@ -0,0 +1,44 @@
+const harina = "400g harina";
+const agua = "300ml agua";
+const aceite = "2 cucharadas aceite"
+const levadura = "15ml levadura"; 
+
+let ingrediente1= "salsa de tomate";
+let ingrediente2= "pollo";
+let ingrediente3= "champiñones";
+let ingrediente4= "queso mozzarela"
+
+console.log(harina);
+function mezcla()
+{
+    console.log("añadir a un bowl una pizca de sal y azucar" +" "+ agua + " "+ aceite + " "+ harina + ", mezclar durante un minuto ");
+}
+function tomarRodillo()
+{
+    console.log("tomar un rodillo ");
+}
+function amasar()
+{
+    tomarRodillo();
+    console.log("y amasar hasta obtener la textura sin grumos");
+}
+function calentarHorno()
+{
+    console.log("poner el horno a 220 grados");
+}
+function añadirSalsaTomate()
+{
+    console.log("añadir: "+ ingrediente1);
+}
+function añadirIngredientesMasa()
+{
+    
+    console.log("Añadimos queso rallado");
+    console.log("Añadimos pollito y champiñones");
+    console.log("añadimos especias");
+}
+function meterPizzaHorno()
+{
+
+
+}
