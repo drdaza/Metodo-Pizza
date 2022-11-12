207ff84a6b38c8a1a4be01a845313611b383aa3c
David Daza <drdaza18@gmail.com>
2022-11-12 22:32:23 +0100

    using module- menu of pizzas

--- app.js
+++ app.js
@@ -5,25 +5,50 @@ const yeast = "15ml levadura";
 const sugar ="a litle sugar"
 const salt = "a litle salt" 
 const MassBasse= [flour, water, oil, yeast, sugar, salt]
-
-const readLineSync = require('readline-sync')
-
 let userRes;
-
-userRes = readLineSync.question("Pick an option");
-console.log(userRes);
-    
+const readLineSync = require('readline-sync');
 
 
+let pizzaChickenWithMushrooms = ["salsa de tomate", "pollo", "champiñones", "queso mozzarela"];
+let pizzaPeperoni =["salsa de tomate", "peperoni", "queso mozarella"];
+let pizzaBacon = ["salsa de tomate","bacon", "atun", "queso mozarella"]
 
+makePizza();
 
+function makePizza(){
+    let pizza=selectPizza();
+    console.log(pizza);
+    switch(pizza){
+        case "1": 
+            console.log("hola");
+            AddIngredients(pizzaPeperoni);
+            DeliverPizza();
+            break;
+        case "2":
 
-let ingredient1= "salsa de tomate";
-let ingredient2= "pollo";
-let ingredient3= "champiñones";
-let ingredient4= "queso mozzarela"
-let ingredients = [ingredient1, ingredient2, ingredient3, ingredient4];
+            AddIngredients(pizzaChickenWithMushrooms);
+            DeliverPizza();
+            break; 
+        case "3":
 
+            AddIngredients(pizzaBacon);
+            DeliverPizza();
+            break;
+        default: 
+            break;
+    }
+}
+
+function selectPizza(){
+    do{
+        console.log("=====Pizza's menu=====");
+        console.log("1. Peperoni's pizza");
+        console.log("2. Chicken with mushrooms");
+        console.log("3. Bacon with tuna");
+        userRes = readLineSync.question("Pick an option pizza: ");
+    }while(userRes>=4 || userRes<0 || userRes=="");
+    return userRes;
+}
 function PrepareMix(){
     for (let index = 0; index < MassBasse.length; index++) {
         console.log("added "+MassBasse[index]);  
@@ -33,10 +58,11 @@ function PrepareMix(){
 function knead(){
     
     if(PrepareMix()==true){
-        console.log("kneading the dough...")
+        console.log("kneading the dough...\n")
     }
 }
-function AddIngredients(){
+function AddIngredients(ingredients){
+    knead();
     for (let index = 0; index < ingredients.length; index++) 
     {
         console.log("adding: "+ingredients[index]);
@@ -44,9 +70,8 @@ function AddIngredients(){
     }
 }
 function BakePizza(){
-    knead();
-    AddIngredients();
-    console.log("bakking pizza");
+
+    console.log("bakking pizza for 10 minutes... \n");
     return true;
 }
 function DeliverPizza(){
